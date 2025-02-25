04667ca7e1c21f6f513f08cd4741e1cfda1ae8fe
Nirmal-Singh-G <nirmalsinghg.10@gmail.com>
2025-02-25 23:51:22 +0530

    Add method in class A

--- src/com/test/A.java
+++ src/com/test/A.java
@@ -4,4 +4,12 @@ public class A {
 	public void company() {
 		System.out.println("The company name is Vivo");
 	}
+
+	private void anna() {
+		System.out.println("Anna1");
+	}
+
+	private void bro() {
+		System.out.println("Anna2");
+	}
 }
